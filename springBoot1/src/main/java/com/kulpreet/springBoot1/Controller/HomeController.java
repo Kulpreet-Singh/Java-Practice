9898fb724cdac25a8925a0e1cd554b15e5a6d1c5
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2023-01-29 16:05:02 +0530

    added logging in HomeController using slf4j with default logback config

--- springBoot1/src/main/java/com/kulpreet/springBoot1/Controller/HomeController.java
+++ springBoot1/src/main/java/com/kulpreet/springBoot1/Controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.kulpreet.springBoot1.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @RequestMapping(method = RequestMethod.GET, value = "/")
     public String home(){
+        logger.debug("Printing welcome message");
         return ("<h1> Welcome to the Homepage of Employee Department Management Project </h1>");
     }
 }
