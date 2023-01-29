fd11106a1aeba043aaef101e6118c965789abd82
Kulpreet Singh <43897531+Kulpreet-Singh@users.noreply.github.com>
2023-01-29 20:52:14 +0530

    added BCryptPasswordEncoder instead of NoOpPasswordEncoder

--- springBoot1/src/main/java/com/kulpreet/springBoot1/SecurityConfiguration.java
+++ springBoot1/src/main/java/com/kulpreet/springBoot1/SecurityConfiguration.java
@@ -1,31 +1,39 @@
 package com.kulpreet.springBoot1;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("blah").password("blah").roles("USER")
-                .and().withUser("foo").password("foo").roles("ADMIN");
+
+    @Autowired
+    private final PasswordEncoder passwordEncoder;
+
+    public SecurityConfiguration(PasswordEncoder passwordEncoder){
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @Bean
-    protected PasswordEncoder getPasswordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("blah").password(passwordEncoder.encode("blah")).roles("USER")
+                .and().withUser("foo").password(passwordEncoder.encode("foo")).roles("ADMIN");
     }
+
+//    @Bean
+//    protected PasswordEncoder getPasswordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests().antMatchers("/departments", "/admin").hasRole("ADMIN")
-                .and().authorizeRequests().antMatchers("/user").hasAnyRole("USER", "ADMIN")
-                .and().authorizeRequests().antMatchers("/**").permitAll()
+        http.authorizeRequests()
+                .antMatchers("/departments", "/admin").hasRole("ADMIN")
+                .antMatchers("/user").hasAnyRole("USER", "ADMIN")
+                .antMatchers("/").permitAll()
                 .and().formLogin();
     }
 }
--- springBoot1/src/main/java/com/kulpreet/springBoot1/SpringBoot1Application.java
+++ springBoot1/src/main/java/com/kulpreet/springBoot1/SpringBoot1Application.java
@@ -2,12 +2,25 @@ package com.kulpreet.springBoot1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class SpringBoot1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBoot1Application.class, args);
+	}
+
+
+	// Create and configure an instance of a Password encoder to encrypt the users' passwords.
+
+	@Bean
+	public PasswordEncoder getPasswordEncoder(){
+
+		return new BCryptPasswordEncoder(15);
+
 	}
 
 }
